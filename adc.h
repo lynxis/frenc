dd60fd0181689aa467f5bbc046315634c4c23830
Alexander Couzens <lynxis@fe80.eu>
2015-07-23 23:26:36 +0200

    adc.h: add functions

--- adc.h
+++ adc.h
@@ -6,5 +6,7 @@
 #define ADC_DADR0		(*(volatile unsigned char*) (0xfffff8)) /* D/A data register 0 */
 #define ADC_DADR1		(*(volatile unsigned char*) (0xfffff9)) /* D/A data register 1 */
 #define ADC_DACR		(*(volatile unsigned char*) (0xfffffa)) /* D/A control register */
+int enable_dac(uint8_t channel, uint8_t enable);
+void set_dac(uint8_t channel, uint8_t value);
 
 #endif /* ADC_H */
