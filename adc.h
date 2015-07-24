2ab30446c7e58d707ea86244cfda82343f510bd7
Alexander Couzens <lynxis@fe80.eu>
2015-07-24 22:20:41 +0200

    adc.h: add missing include

--- adc.h
+++ adc.h
@@ -2,6 +2,8 @@
 #ifndef ADC_H
 #define ADC_H
 
+#include "common.h"
+
 /* have to add adc because of dublicate register name DACR for PWMX and ADC */
 #define ADC_DADR0		(*(volatile unsigned char*) (0xfffff8)) /* D/A data register 0 */
 #define ADC_DADR1		(*(volatile unsigned char*) (0xfffff9)) /* D/A data register 1 */
