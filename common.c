efbc327022a9ce3dcaebefcb835214e01df337dd
Alexander Couzens <lynxis@fe80.eu>
2015-08-12 03:37:24 +0200

    common.c: implement strlen
    also add type size_t

---
+++ common.c
@@ -0,0 +1,10 @@
+
+#include "common.h"
+
+size_t strlen(const char *s) {
+	size_t ret = 0;
+	while(s)
+		ret++;
+	return ret;
+}
+
--- common.h
+++ common.h
@@ -5,11 +5,14 @@
 typedef unsigned char uint8_t;
 typedef unsigned int uint16_t;
 typedef unsigned long uint32_t;
+typedef unsigned long size_t;
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof (a) / sizeof ((a)[0]))
 #endif
 
 #define BIT(x) (1 << x)
+
+size_t strlen(const char *s);
 
 #endif /* COMMON_H */
