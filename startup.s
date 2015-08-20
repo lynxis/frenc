e6839047aaa61b7e99acfa06ba3036e2ef205053
Alexander Couzens <lynxis@fe80.eu>
2015-08-21 01:14:47 +0200

    startup.s: move back to asm routines. somehow the c version was very very slow.
    not even the copy itself, even the code running was slower than using the asm routine
    modify the asm routine to use the new linker symbol of data section
    start (rom_data)

--- startup.s
+++ startup.s
@@ -9,7 +9,7 @@
         .global _startp
         .global _startm
 
-        .extern _etext                  ; ROM copy of initialized data segment
+        .extern _rom_data               ; ROM copy of initialized data segment
         .extern _data                   ; beginning of initialized data segment
         .extern _edata                  ; end of initialized data segment
         .extern _bss                    ; beginning of uninitialized data segment
@@ -33,7 +33,38 @@ _continue:
 ; Set stack pointer
         mov.l   #_stack,sp
 
+_start2:
+        mov.l   #_data,er0              ; start of RAM .data section
+        mov.l   #_edata,er1             ; end of RAM .data section
+        mov.l   #_rom_data,er2          ; location of ROM .data storage
+data_init_loop:
+        mov.w   @er2,r3                 ; get next word
+        mov.w   r3,@er0                 ; move it
+        adds    #2,er2                  ; increment source ptr
+        adds    #2,er0                  ; increment destination ptr
+        cmp.l   er1,er0                 ; test for done
+        blo     data_init_loop
+
+; zero out .bss ram data area
+        mov.l   #_bss,er0               ; start of RAM .bss section
+        mov.l   #_ebss,er1              ; end of RAM .bss section
+        mov.w   #0,r2
+bss_init_loop:
+        mov.w   r2,@er0                 ; clear memory
+        adds    #2,er0                  ; increment ptr
+        cmp.l   er1,er0                 ; test for done
+        blo     bss_init_loop
+
+; initialize stack area to 'DEAD' to track stack usage
+        mov.l   #_stack_last,er0        ; beginning of RAM .stack section
+        mov.l   #_stack_first,er1       ; end of RAM .stack section
+        mov.w   #0xDEAD,r2              ; marker indicating unused stack
+stack_init_loop:
+        mov.w   r2,@er0                 ; store marker
+        adds    #2,er0                  ; increment ptr
+        cmp.l   er1,er0                 ; test for done
+        blo     stack_init_loop
+
 ; call main
-        jsr     @_init
         jmp     @_main
         .end
